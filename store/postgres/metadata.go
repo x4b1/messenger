a4a82a40433357b94798ebad3854e95ab59e42f5
Xabi <xmartinez1702@gmail.com>
2023-05-25 14:24:54 +0200

    feat: Improve data scanning (#75)

--- store/postgres/metadata.go
+++ store/postgres/metadata.go
@@ -3,7 +3,7 @@ package postgres
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"errors"
+	"fmt"
 )
 
 type metadata map[string]string
@@ -15,10 +15,12 @@ func (m metadata) Value() (driver.Value, error) {
 // Make the Attrs struct implement the sql.Scanner interface. This method
 // simply decodes a JSON-encoded value into the struct fields.
 func (m *metadata) Scan(value any) error {
-	b, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
+	switch v := value.(type) {
+	case []byte:
+		return json.Unmarshal(v, &m)
+	case string:
+		return json.Unmarshal([]byte(v), &m)
 	}
 
-	return json.Unmarshal(b, &m)
+	return fmt.Errorf("scanning metadata: unknown type %T", value)
 }
